931701abf338c33132a5049f24a2bcd851b097c6
Matt Haidet <matthaidet17@marist.com>
2014-10-12 14:50:33 -0400

    changed GUI back Signed-off-by:Matt Haidet <matthaidet17@marist.com>

--- MedievalFPS/src/main/Main.java
+++ MedievalFPS/src/main/Main.java
@@ -1,6 +1,18 @@
 package main;
 
+import com.bulletphysics.collision.shapes.CollisionShape;
 import com.jme3.app.SimpleApplication;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.collision.shapes.CapsuleCollisionShape;
+import com.jme3.bullet.control.CharacterControl;
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.bullet.util.CollisionShapeFactory;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.light.AmbientLight;
+import com.jme3.light.DirectionalLight;
+import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
@@ -18,8 +30,16 @@ import com.jme3.water.SimpleWaterProcessor;
  * test
  * @author normenhansen
  */
-public class Main extends SimpleApplication implements ScreenController {
+public class Main extends SimpleApplication implements ActionListener {
     private Nifty nifty;
+    private Spatial sceneModel;
+    private BulletAppState bulletAppState;
+    private RigidBodyControl landscape;
+    private CharacterControl player;
+    private Vector3f walkDirection = new Vector3f();
+    private boolean left = false, right = false, up = false, down = false;
+    private Vector3f camDir = new Vector3f();
+    private Vector3f camLeft = new Vector3f();
     private NiftyJmeDisplay hudDisplay;
     public static void main(String[] args) {
         Main app = new Main();
@@ -28,29 +48,100 @@ public class Main extends SimpleApplication implements ScreenController {
 
     @Override
     public void simpleInitApp() {
+        bulletAppState = new BulletAppState();
+        stateManager.attach(bulletAppState);
         initializeWorld(); //initialize water processor and other things and loads terrain
         flyCam.setMoveSpeed(30); //i think we all agree that the camera was too damn slow!
-        
+        setUpKeys();
+        setUpLight();
+        sceneModel = assetManager.loadModel("Models/Scenes/scene1.j3o");
+        //sceneModel.setLocalScale(2f);
+        com.jme3.bullet.collision.shapes.CollisionShape sceneShape =
+                CollisionShapeFactory.createMeshShape((Node) sceneModel);
+        landscape = new RigidBodyControl(sceneShape, 0);
+        sceneModel.addControl(landscape);
 
-// attach the nifty display to the gui view port as a processor
-        NiftyJmeDisplay niftyDisplay = new NiftyJmeDisplay(assetManager,
-                inputManager,
-                audioRenderer,
-                guiViewPort);
-        nifty = niftyDisplay.getNifty();
-        nifty.fromXml("Interface/HUD.xml", "start", this);
+        CapsuleCollisionShape capsuleShape = new CapsuleCollisionShape(1.5f, 6f, 1);
+        player = new CharacterControl(capsuleShape, 0.05f);
+        player.setJumpSpeed(20);
+        player.setFallSpeed(30);
+        player.setGravity(30);
+        player.setPhysicsLocation(new Vector3f(-50, 100, 0));
 
-        guiViewPort.addProcessor(niftyDisplay);
-         
-        
-        
+        rootNode.attachChild(sceneModel);
+        bulletAppState.getPhysicsSpace().add(landscape);
+        bulletAppState.getPhysicsSpace().add(player);
+    }
+
+    private void setUpLight() {
+        // We add light so we see the scene
+        AmbientLight al = new AmbientLight();
+        al.setColor(ColorRGBA.White.mult(1.3f));
+        rootNode.addLight(al);
+
+        DirectionalLight dl = new DirectionalLight();
+        dl.setColor(ColorRGBA.White);
+        dl.setDirection(new Vector3f(2.8f, -2.8f, -2.8f).normalizeLocal());
+        rootNode.addLight(dl);
+    }
+
+    /**
+     * We over-write some navigational key mappings here, so we can add
+     * physics-controlled walking and jumping:
+     */
+    private void setUpKeys() {
+        inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
+        inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
+        inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_W));
+        inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_S));
+        inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
+        inputManager.addListener(this, "Left");
+        inputManager.addListener(this, "Right");
+        inputManager.addListener(this, "Up");
+        inputManager.addListener(this, "Down");
+        inputManager.addListener(this, "Jump");
+    }
+
+    /**
+     * These are our custom actions triggered by key presses. We do not walk
+     * yet, we just keep track of the direction the user pressed.
+     */
+    public void onAction(String binding, boolean isPressed, float tpf) {
+        if (binding.equals("Left")) {
+            left = isPressed;
+        } else if (binding.equals("Right")) {
+            right = isPressed;
+        } else if (binding.equals("Up")) {
+            up = isPressed;
+        } else if (binding.equals("Down")) {
+            down = isPressed;
+        } else if (binding.equals("Jump")) {
+            if (isPressed) {
+                player.jump();
+            }
+        }
     }
     @Override
     public void simpleUpdate(float tpf) {
-        
-        //TODO: add update code
-        
+        camDir.set(cam.getDirection()).multLocal(0.6f);
+        camLeft.set(cam.getLeft()).multLocal(0.4f);
+        walkDirection.set(0, 0, 0);
+        if (left) {
+            walkDirection.addLocal(camLeft);
+        }
+        if (right) {
+            walkDirection.addLocal(camLeft.negate());
+        }
+        if (up) {
+            walkDirection.addLocal(camDir);
+        }
+        if (down) {
+            walkDirection.addLocal(camDir.negate());
+        }
+        player.setWalkDirection(walkDirection);
+        cam.setLocation(player.getPhysicsLocation());
     }
+    
 
     @Override
     public void simpleRender(RenderManager rm) {
@@ -80,5 +171,21 @@ public class Main extends SimpleApplication implements ScreenController {
        Spatial myTerrain = assetManager.loadModel(
                 "Models/Scenes/scene1.j3o");
        rootNode.attachChild(myTerrain);
-   }
+    }
+
+    public void initializeGUI() {
+        /*  // attach the nifty display to the gui view port as a processor
+         NiftyJmeDisplay niftyDisplay = new NiftyJmeDisplay(assetManager,
+                inputManager,
+                audioRenderer,
+                guiViewPort);
+        nifty = niftyDisplay.getNifty();
+
+         nifty.fromXml("Interface/HUD.xml", "start", this);
+
+        guiViewPort.addProcessor(niftyDisplay);
+
+         */
+
+    }
 }
