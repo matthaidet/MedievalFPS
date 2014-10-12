d3dac8455be3e322b84250969ee12b536ba8c728
Matt Haidet <matthaidet17@marist.com>
2014-10-11 23:45:42 -0400

    Added new GUI crosshair element Signed-off-by:Matt Haidet <matthaidet17@marist.com>

--- MedievalFPS/src/main/Player.java
+++ MedievalFPS/src/main/Player.java
@@ -4,21 +4,31 @@
  */
 package main;
 
+import java.util.Vector;
+
 /**
  *
  * @author MattH
  */
 public class Player {
     private int score;
-    private int health;
+    private float health;
+    private int deaths;
     private boolean isLiving;
     private LoadOut loadOut;
     private String name;
     private int remainingRespawnTime;
+    private Vector position;
     
     public Player(Profile profile){
         name = profile.getName();
         this.remainingRespawnTime = 0;
+    }
+
+    public void spawn(Vector p) {
+        position = p;
+        isLiving = true;
+        health = loadOut.getHealthStat();
     }
     
 }
