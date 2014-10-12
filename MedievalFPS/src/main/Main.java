454f9447eb213eaffa5dea0b2c017806c660474d
Leon <Leon@LeonDesktop>
2014-10-12 11:20:16 -0400

    lots of failed attempts at water

--- MedievalFPS/src/main/Main.java
+++ MedievalFPS/src/main/Main.java
@@ -6,8 +6,12 @@ import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 import com.jme3.scene.shape.Box;
 import com.jme3.scene.Spatial;
+import com.jme3.util.SkyFactory;
+import com.jme3.water.WaterFilter;
+import com.jme3.water.SimpleWaterProcessor;
 
 /**
  * test
@@ -15,6 +19,11 @@ import com.jme3.scene.Spatial;
  */
 public class Main extends SimpleApplication {
 
+    Spatial waterPlane;
+    Geometry lightSphere;
+    SimpleWaterProcessor waterProcessor;
+    Node sceneNode;
+    
     public static void main(String[] args) {
         Main app = new Main();
         app.start();
@@ -23,22 +32,34 @@ public class Main extends SimpleApplication {
     @Override
     public void simpleInitApp() {
         flyCam.setMoveSpeed(30); //i think we all agree that the camera was too damn slow!
-        
         Spatial myTerrain = assetManager.loadModel(
                 "Models/Scenes/scene1.j3o");
         rootNode.attachChild(myTerrain);
+        rootNode.attachChild(SkyFactory.createSky(assetManager, "Textures/skybox1.dds", false));
+        sceneNode = new Node("Scene");
         
-        //Box b = new Box(1, 1, 1);
-        //Geometry geom = new Geometry("Box", b);
-        //Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        //mat.setColor("Color", ColorRGBA.Blue);
-        //geom.setMaterial(mat);
-        //rootNode.attachChild(geom); 
-    }
+        rootNode.attachChild(sceneNode);
+        //WATER
+        waterProcessor = new SimpleWaterProcessor(assetManager);
+        waterProcessor.setReflectionScene(sceneNode);
+        waterProcessor.setDebug(false);
+        viewPort.addProcessor(waterProcessor);
 
+
+        //create water quad
+       
+        waterPlane=(Spatial)  assetManager.loadModel("Models/Water.mesh.xml");
+        waterPlane.setMaterial(waterProcessor.getMaterial());
+        waterPlane.setLocalScale(40);
+        waterPlane.setLocalTranslation(-5, 0, 5);
+        waterPlane.setMaterial(waterProcessor.getMaterial());
+        rootNode.attachChild(waterPlane);
+        
+    }
     @Override
     public void simpleUpdate(float tpf) {
         //TODO: add update code
+        
     }
 
     @Override
