ff34ec084d3e2e57a71ad4a6c398e2f531105268
Leon <Leon@LeonDesktop>
2014-10-12 12:11:15 -0400

    I MADE THE WATER WORK!

--- MedievalFPS/src/main/Main.java
+++ MedievalFPS/src/main/Main.java
@@ -1,21 +1,17 @@
 package main;
 
 import com.jme3.app.SimpleApplication;
-import com.jme3.material.Material;
-import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
-import com.jme3.scene.shape.Box;
 import com.jme3.scene.Spatial;
 import com.jme3.niftygui.NiftyJmeDisplay;
-import com.jme3.scene.shape.Sphere;
+import com.jme3.post.FilterPostProcessor;
 import de.lessvoid.nifty.Nifty;
 import de.lessvoid.nifty.screen.Screen;
 import de.lessvoid.nifty.screen.ScreenController;
 import com.jme3.util.SkyFactory;
-import com.jme3.water.WaterFilter;
 import com.jme3.water.SimpleWaterProcessor;
 
 /**
@@ -25,11 +21,6 @@ import com.jme3.water.SimpleWaterProcessor;
 public class Main extends SimpleApplication implements ScreenController {
 
     private Nifty nifty;
-    Spatial waterPlane;
-    Geometry lightSphere;
-    SimpleWaterProcessor waterProcessor;
-    Node sceneNode;
-    private Vector3f lightPos =  new Vector3f(33,12,-29);
     public static void main(String[] args) {
         Main app = new Main();
         app.start();
@@ -54,33 +45,17 @@ public class Main extends SimpleApplication implements ScreenController {
         // attach the nifty display to the gui view port as a processor
         guiViewPort.addProcessor(niftyDisplay);
         
-    
-
         rootNode.attachChild(SkyFactory.createSky(assetManager, "Textures/skybox1.dds", false));
-        sceneNode = new Node("Scene");
         
-        rootNode.attachChild(sceneNode);
-        //WATER
-        waterProcessor = new SimpleWaterProcessor(assetManager);
-        waterProcessor.setReflectionScene(sceneNode);
-        waterProcessor.setDebug(false);
-        viewPort.addProcessor(waterProcessor);
-        //create water quad
-       waterPlane = waterProcessor.createWaterGeometry(100, 100);
-        waterPlane=(Spatial)  assetManager.loadModel("Models/Water.mesh.xml");
-        waterPlane.setMaterial(waterProcessor.getMaterial());
-        waterPlane.setLocalScale(256);
-        waterPlane.setLocalTranslation(-5, 5, 5);
-        waterProcessor.setLightPosition(lightPos);
-        waterPlane.setMaterial(waterProcessor.getMaterial());
-        rootNode.attachChild(waterPlane);
-        
-        
+         
+        FilterPostProcessor water;
+        water = assetManager.loadFilter("Models/waterFilter.j3f");
+        viewPort.addProcessor(water);
         
     }
     @Override
     public void simpleUpdate(float tpf) {
-        waterProcessor.setLightPosition(lightPos);
+        
         //TODO: add update code
         
     }
