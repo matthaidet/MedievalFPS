10bb10abc587cca4461d8dbbb0c45ec89aed2f23
Matt Haidet <matthaidet17@marist.com>
2014-10-13 11:32:07 -0400

    Added new Map Interface Signed-off-by:Matt Haidet <matthaidet17@marist.com>

---
+++ MedievalFPS/src/main/maps/DesertIsland.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.maps;
+
+import com.jme3.math.Vector3f;
+import main.Map;
+
+/**
+ *
+ * @author MattH
+ */
+public class DesertIsland implements Map {
+
+    public String getTerrainFilePath() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getDecorationsFilePath() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getBoundriesFilePath() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getWaterFilePath() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getMiniMapFilePath() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Vector3f[] getTeamOneSpawnPoints() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Vector3f[] getTeamTwoSpawnPoints() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Vector3f[] getNeutralSPawnPoints() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public int getPlayerCapacity() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
