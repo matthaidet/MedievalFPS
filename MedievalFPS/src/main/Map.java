0e25bff55ce52ef43c017855ce77af77c9098e40
Matt Haidet <matthaidet17@marist.com>
2014-10-13 11:02:00 -0400

    Added new Map Interface Signed-off-by:Matt Haidet <matthaidet17@marist.com>

--- MedievalFPS/src/main/Map.java
+++ MedievalFPS/src/main/Map.java
@@ -4,10 +4,29 @@
  */
 package main;
 
+import com.jme3.math.Vector3f;
+
 /**
  *
  * @author MattH
  */
-public class Map {
-    static private String sceneFilePath;
+public interface Map {
+
+    public String getTerrainFilePath();
+
+    public String getDecorationsFilePath();
+
+    public String getBoundriesFilePath();
+
+    public String getWaterFilePath();
+
+    public String getMiniMapFilePath();
+
+    public Vector3f[] getTeamOneSpawnPoints();
+
+    public Vector3f[] getTeamTwoSpawnPoints();
+
+    public Vector3f[] getNeutralSPawnPoints();
+
+    public int getPlayerCapacity();
 }
