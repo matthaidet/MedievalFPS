4414a0dfc877009a2d507aff6deae9c0508b82f3
lbro23 <leonbreaux23@gmail.com>
2014-10-17 22:04:55 -0400

    more twerk on arrow physics

--- MedievalFPS/src/main/Main.java
+++ MedievalFPS/src/main/Main.java
@@ -68,8 +68,4 @@ public class Main extends SimpleApplication {
     public Camera getCam() {
         return cam;
     }
-    
-    int arrowDropRateCalculator() { //just here for temporary testing, dont be mad
-        return 3;
-    }
 }
